fb47253225c475b5bb34206d95ff685b59dd86e2
kaati <kaatipuola@gmail.com>
2024-09-03 21:25:16 +0300

    Add page_analyzer/validator.py

---
+++ page_analyzer/validator.py
@@ -0,0 +1,17 @@
+from urlib.parse import urlparse
+from validators import url as is_url_valid
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    normalized_url = f'{parsed_url.scheme}://{parsed_url.hostname}'
+    return normalized_url
+
+
+def validate(url):
+    if not url:
+        return 'URL обязателен для заполнения'
+    if len(url) > 255:
+        return 'Введенный URL превышает длину в 255 символов'
+    if not is_url_valid(url):
+        return 'Некорректный URL'
